7974fb7f6f065283cd06bb605a55bff9ecf294e1
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-08 00:12:46 -0800

    Refactor database query to respond with question data ordered by votes

--- database/database.js
+++ database/database.js
@@ -31,18 +31,24 @@ const customerQuestionsSchema = new mongoose.Schema({
 const CustomerQuestions = mongoose.model('CustomerQuestions', customerQuestionsSchema);
 
 const load = (productId, callback) => {
-  CustomerQuestions.find({ productId: productId })
+  CustomerQuestions
+    .aggregate([
+      { $match: { productId: productId }},
+      { $unwind: '$questionAndAnswers' },
+      { $sort: { 'questionAndAnswers.rating': -1 }},
+      { $group: { _id: '$_id',
+        productId: { $first: '$productId'},
+        questionAndAnswers: { $push: '$questionAndAnswers'}}
+      }
+    ])
     .exec((err, data) => {
       // if error, return error
       if (err) {
-        console.log(`Error loading product ${productId} from database`, err);
         callback(err);
       }
       // else if product does not exist, create and send error
-      // db.collections.find() does not return error when no query match
       else if (data[0] === undefined || !data[0].productId) {
-        console.log(`Error: product ${productId} does not exist`);
-        callback(new Error('Product not found!'));
+        callback(new Error(`Product ${productId} not found!`));
       }
       // else document record at productId exists, so send data
       else {
