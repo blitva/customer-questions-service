550cb66ebbbd1db02daf6d303d60bc7798721b4e
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-13 00:17:33 -0800

    [WIP] search view and pop up modal

--- client/components/SearchView.jsx
+++ client/components/SearchView.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import PostQuestionModal from './PostQuestionModal.jsx';
 
 const SearchViewWrapper = styled.div`
   display: block;
@@ -73,8 +74,72 @@ const Header = styled.h1`
 
 // const CustomerReviewsResults = stlyed.div``;
 
-const SearchView = () => {
+const BottomQuestionContainer = styled.div`
+  margin-top: 32px;
+  margin-bottom: 32px;
+`;
 
+const PostQuestionContainer = styled.div`
+  border: 1px #F0F2F2 solid;
+  background-color: #F0F2F2!important
+`;
+
+const Question = styled.div`
+  padding-top: 18px;
+  padding-right: 22px;
+  padding-bottom: 18px;
+  padding-left: 22px;
+  font-weight: 700!important;
+  font-size: 14px!important;
+`;
+
+const Button = styled.button`
+  width: 131px;
+  height: 30px;
+  margin-left: 14px;
+  background: #e7e9ec;
+  border-radius: 3px;
+  border-color: #ADB1B8 #A2A6AC #8D9096;
+  border-stlye: solid;
+  border-width: 1px;
+  text-align: center;
+  text-decoration: none!important;
+  vertical-algin: middle;
+  &:focus {
+    outline: none;
+    border-color: #e77600;
+    box-shadow: 0 0 3px 2px rgb(228 121 17 / 50%);
+  }
+`;
+
+const AlsoAskedWidget = styled.div`
+  display: block;
+`;
+
+const FollowUpQuestion = styled.span`
+  display: inline-block;
+  height: 30px;
+  max-width: 100%;
+  padding 0 14px;
+  margin 0 10px 14px 0;
+  background-color: #EBF8FA;
+  border-bottom: solid 1px #969696;
+  color: #002F36;
+  cursor: pointer;
+`;
+
+const QuestionLink = styled.a`
+  line-height: 30px;
+  text-align: center;
+`;
+
+
+const SearchView = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = (e) => {
+    setShowModal(!showModal);
+  }
 
   return (
     <SearchViewWrapper>
@@ -117,9 +182,39 @@ const SearchView = () => {
             <Link>See 20 matching customer reviews</Link>
         </LinkWrapper>
 
+        <BottomQuestionContainer>
+          <PostQuestionContainer>
+            <Question>
+            Don't see the answer you're looking for?
+            <Button onClick={toggleModal}>Post your question</Button>
+            </Question>
+          </PostQuestionContainer>
+        </BottomQuestionContainer>
       </SearchResultsContianer>
 
+      <AlsoAskedWidget>
+        <Header>Customers also asked</Header>
+        <FollowUpQuestion>
+          <QuestionLink>
+            is there a monthly service fee?
+          </QuestionLink>
+          </FollowUpQuestion>
 
+          <FollowUpQuestion>
+            <QuestionLink>
+              Esta Bosina es Recagable
+            </QuestionLink>
+          </FollowUpQuestion>
+
+          <FollowUpQuestion>
+            <QuestionLink>
+              How to connect this device via alexa app if network is hidden? i can’t find the option to manual add domain.
+            </QuestionLink>
+          </FollowUpQuestion>
+      </AlsoAskedWidget>
+      <PostQuestionModal
+        show={showModal}
+      />
     </SearchViewWrapper>
   )
 }
